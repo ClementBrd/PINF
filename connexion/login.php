fe631124bb91a3c52f66b41b53d0cc41c8ea059f
Borobo <37670395+Borobo@users.noreply.github.com>
2019-02-09 10:47:47 +0100

    Update login.php

--- connexion/login.php
+++ connexion/login.php
@@ -5,14 +5,25 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="../bootstrap-4.2.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../libs/bootstrap.css">
 
     <script src="../libs/jquery-3.3.1.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
     <script src="../libs/bootstrap.min.js"></script>
 
     <script type="text/javascript">
-        
+        $(document).on("click", "input[type=submit]", function(){
+            $.getJSON("data.php", 
+            {
+                action : "connexion",
+                identifiant : $("input[type=text]").val(),
+                password : $("input[type=password]").val()
+            }, function(oRep){
+                console.log($("input[type=text]").val());
+                console.log($("input[type=password]").val());
+                console.log(oRep.feedback);
+            })
+        })
     </script>
 </head>
 <body>
@@ -28,7 +39,6 @@
 
         </div>
         <div class="col-sm-4 jumbotron text-center alert-dark">
-            <form action="verification.php" method="GET">
                 <h1>Formulaire</h1><br>
 
                 <input type="text" placeholder="Nom d'utilisateur" name="username" required><br><br><!-- le required permet d'être obligé de rentrer une donnée-->
